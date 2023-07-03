d076ea569146df3d173c2c6825f94b64a7dd633f
IvanBasss <122455382+Basiuk-Ivan@users.noreply.github.com>
2023-07-03 15:19:54 +0300

    first commit

---
+++ src/utils/mail.js
@@ -0,0 +1,55 @@
+import nodemailer from 'nodemailer';
+import HTML_TEMPLATE from './mailTemplate.js';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export const sendMailOrder = async ({ products, email, totalPrice }) => {
+  const productsList = products.map(
+    ({ name, price, quantity }) =>
+      `<li>${name} ціна: ${price} грн    кількість: ${quantity} шт  разом: ${
+        price * quantity
+      } грн </li>`
+  );
+  const message = `<p>Привіт, шановний клієнт. Це ваше замовлення </p>
+        <ul>${productsList}</ul>
+        <hr>
+        <div style="text-align: right">РАЗОМ:  ${totalPrice} грн</div>
+        `;
+  await transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to: email,
+    subject: 'Ваше замовлення від сайту Pharmacy ',
+    text: 'message',
+    html: HTML_TEMPLATE(message),
+  });
+};
+
+export const sendMailRegistration = async ({
+  email,
+  firstName,
+  secondName,
+  password,
+}) => {
+  const message =
+    'Привіт, шановний(на) ' +
+    firstName +
+    ' ' +
+    secondName +
+    '.Ваша реєстрація успішна. Ваша пошта: ' +
+    email +
+    ', Ваш пароль: ' +
+    password;
+  await transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to: email,
+    subject: 'Ваші реєстраційні дані на сайті Pharmacy ',
+    text: message,
+    html: HTML_TEMPLATE(message),
+  });
+};
